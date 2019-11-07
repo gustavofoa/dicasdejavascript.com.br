41c70a3b6ab4a14e2e13e6616e1b211d7d340998
Gustavo Furtado de Oliveira Alves <gustavo.alves@gsw.com.br>
2019-11-07 18:20:09 -0300

    create component Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>&#123; Dicas de Javascript &#125;</h1>
+        <p>
+            Dicas práticas e tutorias para solucionar as suas dǘvidas de Javascript!
+        </p>
+    </div>
+)
+
+export default Profile
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Profile from "./Profile"
 
 const Layout = ({ children }) => {
   return (
+    <>
+    <aside>
+      <Profile></Profile>
+    </aside>
     <main>{children}</main>
+    </>
   )
 }
 
