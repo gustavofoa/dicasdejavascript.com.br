fb9bfd24570ed7807e77c27e417212ed14e2b8c9
Gustavo Furtado de Oliveira Alves <gustavo.alves@gsw.com.br>
2019-11-08 12:09:13 -0300

    MenuLinks

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import * as S from "./styled"
 import BlogInfo from "../BlogInfo"
+import MenuLinks from '../MenuLinks'
 
 const Sidebar = () => (
     <S.SidebarWrapper>
         <BlogInfo />
+        <MenuLinks />
     </S.SidebarWrapper>
 )
 
---
+++ src/pages/sobre.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Sobre = () => (
+    <Layout>
+        <h1>Sobre</h1>
+
+    </Layout>
+)
+
+export default Sobre;
