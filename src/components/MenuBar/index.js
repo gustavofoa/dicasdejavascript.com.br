3ff29aaa04e208063969f42c7a668f1535e06461
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2020-01-18 16:35:34 -0300

    remove grid dependency.

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -4,7 +4,6 @@ import { Home } from "styled-icons/boxicons-solid/Home"
 import { SearchAlt2 as Search } from "styled-icons/boxicons-regular/SearchAlt2"
 import { UpArrowAlt as Arrow } from "styled-icons/boxicons-regular/UpArrowAlt"
 import { Lightbulb as Light } from "styled-icons/fa-regular/Lightbulb"
-import { Grid } from "styled-icons/boxicons-solid/Grid"
 
 import * as S from "./styled"
 
